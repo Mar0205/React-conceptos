ed430ff51605354ad295ad205f9630951184aae2
Mariana <molarte92@misena.edu.co>
2020-11-05 07:25:58 -0500

    github

---
+++ src/Components/App.jsx
@@ -0,0 +1,39 @@
+function App() {
+  let mensaje = "Hola mundo";
+  return (
+    <section className="bg-dark text-light" >
+      <div className="container py-5">
+       <h2 className= "mb-4">Conceptos básicos de react</h2>
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-3 col-xl-3 mb-4">
+            <article className="bg-info p-3">
+              <h4>Lorem ipsum dolor sit.</h4>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, illo?</p>
+            </article>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-3 col-xl-3 mb-4">
+            <article className="bg-info p-3">
+              <h4>Lorem ipsum dolor sit.</h4>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, illo?</p>
+            </article>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-3 col-xl-3 mb-4">
+            <article className="bg-info p-3">
+              <h4>Lorem ipsum dolor sit.</h4>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, illo?</p>
+            </article>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-3 col-xl-3 mb-4">
+            <article className="bg-info p-3">
+              <h4>Lorem ipsum dolor sit.</h4>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, illo?</p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  );
+}
+
+export default App;
